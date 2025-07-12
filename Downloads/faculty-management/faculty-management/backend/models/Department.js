819cf18a7d5fe8b448df71e998432525850b15e1
venithaaselvaraj <155301880+venithaaselvaraj@users.noreply.github.com>
2025-07-12 18:14:53 +0530

    Delete Downloads/faculty-management/faculty-management/backend directory

--- Downloads/faculty-management/faculty-management/backend/models/Department.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DepartmentSchema = new mongoose.Schema({
-  name: String,
-  hod: String,
-  building: String
-});
-
-module.exports = mongoose.model('Department', DepartmentSchema);
